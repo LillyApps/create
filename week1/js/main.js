ebd2074a15fa0122cee397171da710c1faa6402b
Benedicte Raae <raae@bgraphic.no>
2014-02-26 23:52:52 +0100

    Filling up page with photos tagged 'flytteekser'

--- week1/js/main.js
+++ week1/js/main.js
@@ -1,1 +1,32 @@
+var CLIENT_ID = '3c52889feb714456b62ba61fe7add54b';
 
+function parseAndRenderInstagramResponse(response) {
+  addPhotosToDOM(response.data);
+}
+
+function addPhotosToDOM(photos) {
+  var photosEl = document.getElementById('photos');
+  
+  for (var photoIdx in photos)
+  {
+    var photo = photos[photoIdx];
+    
+    var imgEl = document.createElement('img');
+    imgEl.src = photo.images.low_resolution.url;
+    photosEl.appendChild(imgEl);   
+  }
+}
+
+function fetchInstagramPhotos(url) {
+
+   var script = document.createElement('script');
+   script.src = url;
+   document.body.appendChild(script);
+}
+
+function fetchInstagramPhotosWithTag(tag, callbackFunction) {
+  var url = 'https://api.instagram.com/v1/tags/' + tag + '/media/recent/?client_id='+ CLIENT_ID + '&callback=' + callbackFunction;
+  fetchInstagramPhotos(url);
+}
+
+fetchInstagramPhotosWithTag('flytteesker', 'parseAndRenderInstagramResponse');
